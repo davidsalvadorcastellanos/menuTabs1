108dc5e45c51574e1e9484016c6acfbe24cd5977
David Salvador Castellanos <david.salvador.castellanos.dsc@gmail.com>
2025-12-08 16:38:30 +0100

    Ejercicio 1

---
+++ app/src/main/java/com/example/menutabs1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.menutabs1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tabLayout: TabLayout = findViewById(R.id.tabs)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            lateinit var fragment : Fragment
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> fragment = Fragmento1()
+                    1 -> fragment = Fragmento2()
+                    2 -> fragment = Fragmento3()
+                }
+                supportFragmentManager.beginTransaction().replace(R.id.content_fragment, fragment).commit()
+                supportActionBar?.setTitle(tab.text)
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {}
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+        })
+        }
+    }
