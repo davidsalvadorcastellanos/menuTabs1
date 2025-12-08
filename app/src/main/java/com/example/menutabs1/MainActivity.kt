4132e7594a8eaf3a68079d1911faebee0f48c8cd
David Salvador Castellanos <david.salvador.castellanos.dsc@gmail.com>
2025-12-08 16:54:10 +0100

    Ejercicio 1 - Correcion titulo

--- app/src/main/java/com/example/menutabs1/MainActivity.kt
+++ app/src/main/java/com/example/menutabs1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.menutabs1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = "My Application"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,7 +36,6 @@ class MainActivity : AppCompatActivity() {
                     2 -> fragment = Fragmento3()
                 }
                 supportFragmentManager.beginTransaction().replace(R.id.content_fragment, fragment).commit()
-                supportActionBar?.setTitle(tab.text)
             }
 
             override fun onTabUnselected(p0: TabLayout.Tab?) {}
